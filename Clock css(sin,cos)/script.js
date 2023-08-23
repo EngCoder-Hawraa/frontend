28e1d6d59820b74b494264d42048fe6975a6025f
Hawraa Arkan <52410420+EngCoder-Hawraa@users.noreply.github.com>
2023-08-23 14:04:52 +0300

    new js

--- Clock css(sin,cos)/script.js
+++ Clock css(sin,cos)/script.js
@@ -4,3 +4,43 @@ const hour = -3600 * (time.getHours() % 12);
 const mins = -60 * time.getMinutes();
 app.style.setProperty('--_dm', `${mins}s`);
 app.style.setProperty('--_dh', `${(hour+mins)}s`);
+
+
+// Window Size and Dimensions
+console.log(window.innerWidth);
+console.log(window.innerHeight);
+console.log(window.outerWidth);
+console.log(window.outerHeight);
+
+// Window Location
+console.log(window.location);
+console.log(window.location.href);
+// console.log(window.location.reload());
+
+// Window History
+console.log(window.history);
+console.log(window.history.back());
+console.log(window.history.forward());
+
+// Window Navigator
+console.log(window.navigator);
+
+// Window Timers
+// console.log(window.setTimeout(function, delay));
+// console.log(window.setInterval(function, interval));
+
+// Window Popup and Alerts
+// console.log(window.alert(message));
+// console.log(window.confirm(message));
+// console.log(window.prompt(message, default));
+
+// Window Opening and Closing
+// console.log(window.open(url, name, specs));
+// console.log(window.close());
+
+
+// Window Screen
+
+
+console.log(window.navigator.userAgent);
+console.log(window.navigator.userAgent);
---
+++ Sticky Notes App using Javascript/script.js
@@ -0,0 +1,16 @@
+let createBox = document.getElementsByClassName('createBox')[0];
+let notes = document.getElementsByClassName('notes')[0];
+let input = document.getElementById('userInput');
+let i = 0;
+
+createBox.addEventListener('keydown', content);
+
+document.getElementById('create').addEventListener("click", function() {
+    createBox.style.display = "block";
+});
+
+function content(e) {
+    if (e.keyCode == '13') {
+        createBox.style.display = "none";
+    }
+}
