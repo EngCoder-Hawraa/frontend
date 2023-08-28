1a3ac247c81853b61013e6a7c13853394bede4e7
Hawraa Arkan <52410420+EngCoder-Hawraa@users.noreply.github.com>
2023-08-28 11:20:00 +0300

    update

--- Memory Game JS [Korsat X Parmaga]/assets/js/main.js
+++ Memory Game JS [Korsat X Parmaga]/assets/js/main.js
@@ -1,69 +1,65 @@
-// Made By: Korsat X Parmaga :)
-// Find Us on YouTube for more Tutorials
-// Hope you enjoyed, Goodby.
-
 const allCards = document.querySelectorAll(".card");
 
 let firstCard = null,
-  secondCard = null;
+    secondCard = null;
 
 let canClick = true;
 
 let score = 0;
 
 allCards.forEach((card) => {
-  card.addEventListener("click", handleCardClicked);
+    card.addEventListener("click", handleCardClicked);
 });
 
 // handle clicking on a card
 function handleCardClicked() {
-  if (!canClick) return;
+    if (!canClick) return;
 
-  // prevent card double click
-  if (this.classList.contains("flip")) return;
+    // prevent card double click
+    if (this.classList.contains("flip")) return;
 
-  this.classList.add("flip");
+    this.classList.add("flip");
 
-  if (!firstCard) firstCard = this;
-  else if (!secondCard) secondCard = this;
+    if (!firstCard) firstCard = this;
+    else if (!secondCard) secondCard = this;
 
-  let img1 = firstCard ? firstCard.firstElementChild.src : null;
-  let img2 = secondCard ? secondCard.firstElementChild.src : null;
+    let img1 = firstCard ? firstCard.firstElementChild.src : null;
+    let img2 = secondCard ? secondCard.firstElementChild.src : null;
 
-  // handle matching condition
-  if (img1 === img2) {
-    console.log("Matching....");
-    firstCard = null;
-    secondCard = null;
+    // handle matching condition
+    if (img1 === img2) {
+        // console.log("Matching....");
+        firstCard = null;
+        secondCard = null;
 
-    score++;
-    if (score === 6) handleGameOver();
-  }
-  // handle non-matching condition
-  else if (img1 && img2) {
-    canClick = false;
+        score++;
+        if (score === 6) handleGameOver();
+    }
+    // handle non-matching condition
+    else if (img1 && img2) {
+        canClick = false;
 
-    setTimeout(() => {
-      firstCard.classList.remove("flip");
-      secondCard.classList.remove("flip");
-      firstCard = null;
-      secondCard = null;
-      canClick = true;
-    }, 1000);
-  }
+        setTimeout(() => {
+            firstCard.classList.remove("flip");
+            secondCard.classList.remove("flip");
+            firstCard = null;
+            secondCard = null;
+            canClick = true;
+        }, 1000);
+    }
 }
 
 function handleGameOver() {
-  setTimeout(() => {
-    let retVal = confirm("You Win 😍😍\nPlay Again ???");
-    if (retVal === true) {
-      location.reload();
-    }
-  }, 1000);
+    setTimeout(() => {
+        let retVal = confirm("لقد فزت 😍😍\n تلعب مجددا ؟؟؟");
+        if (retVal === true) {
+            location.reload();
+        }
+    }, 1000);
 }
 
 // ====== Shuffle Cards =========
 allCards.forEach((card) => {
-  let randPos = Math.floor(Math.random() * 12);
-  card.style.order = randPos;
+    let randPos = Math.floor(Math.random() * 12);
+    card.style.order = randPos;
 });
